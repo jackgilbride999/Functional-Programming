abef35cb78975cbe7f444e30984560f3b9f487f1
jackgilbride999 <jackgilbride999@gmail.com>
2019-09-29 18:46:09 +0100

    Added folder for Exercise01

---
+++ CSU34016-1920/Exercise01/src/Ex01.hs
@@ -0,0 +1,30 @@
+-- Name: An On,  Username: anon
+module Ex01 where
+import Data.Char (toUpper) -- needed for Part 1
+
+{- Part 1
+
+Write a function 'raise' that converts a string to uppercase
+
+Function 'toUpper :: Char -> Char' converts a character to uppercase
+if it is lowercase. All other characters are unchanged
+
+-}
+raise _ = error "raise not yet implemented"
+
+{- Part 2
+
+Write a function 'nth' that returns the nth element of a list
+
+-}
+nth :: Int -> [a] -> a
+nth _ _ = error "raise not yet implemented"
+
+{- Part 3
+
+write a function commonLen that compares two sequences
+and reports the length of the prefix they have in common.
+
+-}
+commonLen :: Eq a => [a] -> [a] -> Int
+commonLen _ _ = error "commonLen not yet implemented"
