870da2e7e8cf4438f7400097d2da831156cc3297
jackgilbride999 <jackgilbride999@gmail.com>
2019-10-21 12:00:42 +0100

    Added comments for improved readability and understanding

--- CSU34016-1920/Exercise02/src/Ex02.hs
+++ CSU34016-1920/Exercise02/src/Ex02.hs
@@ -1,4 +1,4 @@
-{- butrfeld Andrew Butterfield -}
+{- gilbridj Jack Joseph Gilbride -}
 module Ex02 where
 import Data.List ((\\))
 
@@ -6,8 +6,10 @@ import Data.List ((\\))
 
 -- do not change anything in this section !
 
+-- Any time we say Id, we can effectively substitute it with String
 type Id = String
 
+-- Expr data type
 data Expr
   = Val Double
   | Add Expr Expr
@@ -18,18 +20,24 @@ data Expr
   | Def Id Expr Expr
   deriving (Eq, Show)
 
+-- a dictionary maps keys to datum values
+-- an obvious approach is to use a list of key/datum pairs
 type Dict k d  =  [(k,d)]
 
+-- defining a link between a key and datum is simply cons-ing such a pair onto the start of the list
 define :: Dict k d -> k -> d -> Dict k d
 define d s v = (s,v):d
 
+-- find simply searches along the list
 find :: Eq k => Dict k d -> k -> Maybe d
 find []             _                 =  Nothing
 find ( (s,v) : ds ) name | name == s  =  Just v
                          | otherwise  =  find ds name
 
+-- a dictionary where the key is a string and the datum is a double
 type EDict = Dict String Double
 
+-- sample data
 v42 = Val 42 ; j42 = Just v42
 
 -- Part 1 : Evaluating Expressions -- (60 test marks, worth 15 Exercise Marks) -
