2448a47a381e495a81f970ff82a4bdb6b9f04367
Jack Gilbride <jackgilbride999@gmail.com>
2019-11-14 12:39:50 +0000

    Added further comments in Ex03.hs to make functions more understandable

--- CSU34016-1920/Exercise03/src/Ex03.hs
+++ CSU34016-1920/Exercise03/src/Ex03.hs
@@ -40,17 +40,31 @@ lkpBST (Branch left k d right) k'
 -- Coding Part 1 (13 Marks)
 
 -- insert into binary (search) tree
+{-
+    Takes a key of type a, value of type b, and a BST which holds keys of type
+    a and values of type b. Returns the BST with the key value pair inserted.
+    a is retricted to an orderable type so that it can be compared.
+-}
 insBST :: Ord a => a -> b -> BT a b -> BT a b
 insBST _ _ _  =  error "insBST not yet implmented"
 
 -- Coding Part 2 (6 Marks)
 
 -- convert an association list to a binary search tree
+{-
+    Takes an association list and returns an equivalent BST, where both hold keys 
+    of type a and values of type b. a is retricted to an orderable type so that it 
+    can be compared.
+-}
 assoc2bst :: Ord a => Assoc a b -> BT a b
 assoc2bst _ = error "assoc2bst not yet implemented"
 
 -- Coding Part 3 (6 Marks)
 
 -- convert a binary search tree into an (ordered) association list
+{-
+  Takes a BST and returns an equivalent association list, where both hold keys of type
+  c and values of type e. c is retricted to an orderable type so that it can be compared.
+-}
 bst2assoc :: Ord c =>  BT c e -> Assoc c e
 bst2assoc _ = error "bst2assoc not yet implemented"
