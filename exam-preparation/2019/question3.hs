ef64413b12ef072ac3487db4791c09060cab8dba
Jack Gilbride <jackgilbride999@gmail.com>
2019-12-06 13:15:38 +0000

    Finished 2019 3b, attempted 2019 3c

--- exam-preparation/2019/question3.hs
+++ exam-preparation/2019/question3.hs
@@ -17,4 +17,29 @@
 
         LAZY:
             take 2 (quadup 10)
-}
+            take 2 (10 : quadup (14))
+            10 : take 1 (quadup (14))
+            10 : take 1 (14 : quadup (18))
+            10 : (14 : take 0 (quadup (18))
+            10 : (14 : [])
+            10 : [14]
+            [10, 14]
+-}
+
+{-
+    (c) Write a function toDOS that takes a string representing a filename (root.ext) and converts it to one satisfying the DOS 8+3 format (All uppercase, root and extension with max length of 8 and 3 respectively).
+        If the root or extension needs to be shortened, then they should be truncated. For example:
+        toDOS "LongBigName.datafile" = "LONGBIGN.DAT"
+-}
+
+    toDOS :: String -> String
+
+    toDOS (x:xs) = helperDOS (x:xs) 0 false 0 
+
+    helperDOS :: String -> Int -> Bool -> Int -> String
+    helperDOS [] _ _ _ = [] -- an empty string converts to an empty string
+    helperDOS (x:xs) rootN pastDot extN =
+        | pastDot = if (extN < 3) then ((toUpper x) : helperDOS xs rootN pastDot (extN+1)) else []    -- we have passed the dot, if the extension length is <3, continue appending, otherwise, ignore the rest of inpput
+        | otherwise = if (x = '.') then x : (helperDOS xs rootN True extN)  -- if we reach the dot, set dotPassed to true and continue appenfine
+                        else if (rootN < 8) then (toUpper x) : helperDOS xs (rootN + 1) pastDot extN -- if we haven't passed the dot and the root lenght is <8, continue appending
+                            else helperDOS xs rootN pastDot extN                                     -- otherwise do not append but keep going as we must eventually append the dot and extension   
