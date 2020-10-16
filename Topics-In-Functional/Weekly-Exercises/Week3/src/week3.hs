54d99346c2e35d0692b66dd97d39baea43638f2f
Jack Gilbride <jackgilbride999@gmail.com>
2020-10-16 20:37:34 +0100

    First attempt at week3

--- Topics-In-Functional/Weekly-Exercises/Week3/src/week3.hs
+++ Topics-In-Functional/Weekly-Exercises/Week3/src/week3.hs
@@ -1,1 +1,11 @@
 main = putStr "week3"
+
+data List a = Nil | Cons a (List a)
+
+instance Monad List where
+      (>>=) :: List a -> (a -> List b) -> List b
+      Nil >>= k = Nil
+      (Cons a as) >>= k = Cons a (k as)
+
+      return :: a -> List a
+      return a = Cons a
