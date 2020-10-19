0b957cb666b9572fe644c153ea18db409e6dfc25
Jack Gilbride <jackgilbride999@gmail.com>
2020-10-19 10:22:58 +0100

    Possibly correct attempt at part 1. All functions building. Proofs needed

--- Topics-In-Functional/Weekly-Exercises/Week3/src/week3.hs
+++ Topics-In-Functional/Weekly-Exercises/Week3/src/week3.hs
@@ -1,11 +1,27 @@
+import Control.Monad
+
 main = putStr "week3"
 
 data List a = Nil | Cons a (List a)
 
-instance Monad List where
-      (>>=) :: List a -> (a -> List b) -> List b
-      Nil >>= k = Nil
-      (Cons a as) >>= k = Cons a (k as)
+instance Functor (List) where
+     --fmap = liftM
+     -- fmap :: (a -> b) -> f a -> f b
+     fmap f Nil = Nil
+     fmap f (Cons x xs) = Cons (f x) (fmap f xs)
 
-      return :: a -> List a
-      return a = Cons a
+
+instance Applicative (List) where
+     -- pure :: a -> f a
+     pure x = Cons x Nil
+
+     -- (<*>) :: List (a -> b) -> List a -> List b
+     _ <*> Nil = Nil
+     Nil <*> _ = Nil
+     Cons f fs <*> Cons x xs = Cons (f x) (fs <*> xs)
+
+instance Monad (List) where
+     -- (>>=) :: List a -> (a -> List b) -> List b
+      Nil >>= f = Nil
+      (Cons x xs) >>= f = f x
+      
