bf02e1ed54c5f46e9393a781791876dead32669e
Jack Gilbride <jackgilbride999@gmail.com>
2020-11-25 18:23:33 +0000

    Server serves up an image

--- Topics-In-Functional/Assignments/Assignment1/src/Main.hs
+++ Topics-In-Functional/Assignments/Assignment1/src/Main.hs
@@ -8,14 +8,27 @@ import Text.Blaze.Html.Renderer.Text
 import Shapes
 import Render (render,defaultWindow)
 
-main = Scotty.scotty 3000 $ do
+main = 
+ Scotty.scotty 3000 $ do
     Scotty.get "/" $ do
         Scotty.html $ response " world!"
+    
+    Scotty.get "/output.png" $ Scotty.file "./output.PNG"
+
+    --Scotty.get "drawing/:shape" $
+        
+
+
+blaze = Scotty.html . renderHtml
 
 response :: Text -> Text
 response n = do 
     renderHtml $ do
         h1 ("Hello" >> toHtml n)
+        img ! src "output.png"
+        p " [ (scale (point 0.5 0.25) <+> translate (point 1.2 0.4), circle) ]"
+     --   img ! src "drawing/[ (scale (point 0.5 0.25) <+> translate (point 1.2 0.4), circle) ]"
+
 
 
 exampleDrawing =  [ (scale (point 0.5 0.25) <+> translate (point 1.2 0.4), circle) ]
