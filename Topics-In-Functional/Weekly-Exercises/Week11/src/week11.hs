34ac0c85cfc0834798c179b2fc411f143701778b
Jack Gilbride <jackgilbride999@gmail.com>
2020-12-09 19:32:00 +0000

    Calculator layout

--- Topics-In-Functional/Weekly-Exercises/Week11/src/week11.hs
+++ Topics-In-Functional/Weekly-Exercises/Week11/src/week11.hs
@@ -10,9 +10,39 @@
 -}
 import Graphics.UI.Threepenny
 
-
 main = do
- startGUI defaultConfig setup
+ startGUI defaultConfig calculator
+
+calculator :: Window -> UI ()
+calculator window = do 
+    return window # set title "Simple calculator"
+    button1 <- button # set text "1"
+    button2 <- button # set text "2"
+    button3 <- button # set text "3"
+    button4 <- button # set text "4"
+    button5 <- button # set text "5"
+    button6 <- button # set text "6"
+    button7 <- button # set text "7"
+    button8 <- button # set text "8"
+    button9 <- button # set text "9"
+    button0 <- button # set text "0"
+    buttonAdd <- button # set text "+"
+    buttonSub <- button # set text "-"
+    buttonMul <- button # set text "X"
+    buttonDiv <- button # set text "÷"
+    buttonEquals <- button # set text "="
+    buttonClear <- button # set text "C"
+    getBody window #+ [
+        grid [
+            [element button1, element button2, element button3, element buttonAdd],
+            [element button4, element button5, element button6, element buttonSub],
+            [element button7, element button8, element button9, element buttonMul],
+            [element buttonClear,element button0, element buttonEquals, element buttonDiv] 
+            ]
+            ]
+    return ()
+
+
  
 showMessage :: Window -> UI ()
 showMessage window = do
